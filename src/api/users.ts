234d9611a474f09340b2062925186c842419cd2b
william.liu <sloveliu@gmail.com>
2023-09-10 16:08:02 +0800

    feat: 實作登入，並將 form 格式改回符合 api 需求

---
+++ src/api/users.ts
@@ -0,0 +1,25 @@
+// 用戶相關的 api 都寫在這
+import request from "@/utils/request";
+
+// 請求類型
+type LoginInfo = {
+  phone: string;
+  code?: string;
+  password: string;
+};
+// 返回類型
+type LoginResult = {
+  success: boolean;
+  state: number;
+  message: string;
+  content: string;
+};
+
+export const login = (loginInfo: LoginInfo) => {
+  return request<LoginResult>({
+    url: "/front/user/login",
+    method: "POST",
+    data: `phone=${loginInfo.phone}&password=${loginInfo.password}` // 因為請求類型是 application/x-www-form-urlencoded，所以 data 必須用拼接，若是 application/json 則 data: loginInfo 即可
+  });
+
+};;
--- src/views/login/LoginView.vue
+++ src/views/login/LoginView.vue
@@ -1,24 +1,34 @@
 <script setup lang="ts">
+import type { FormInstance, FormRules } from "element-plus";
+import { login } from "@/api/users.ts";
 const form = reactive({
-  name: 'qoo',
-  password: '123456',
+  phone: '18201288771',
+  password: '111111',
 });
 const formRef = ref<FormInstance>(); // 名稱要對應到 ref="formRef"
 const onSubmit = async () => {
-  // 發送驗證，預設回傳 Promise。
+  // 發送驗證，預設回傳 Promise
   await formRef.value?.validate().catch(err => { // 只抓失敗 catch err
     ElMessage.error('表單驗證失敗');
     throw err; // 錯誤往外拋，程式就不會往下執行
     // return new Promise(() => { }); // 不想拋錯出去用這樣攔截
   });
   // 成功發送請求出去
-
+  const { content } = await login(form).then(res => {
+    if (!res.data.success) {
+      ElMessage.error("登入失敗");
+      throw new Error("登入資訊錯誤");
+    }
+    return res.data;
+  });
+  console.log(content);
 };
 // 表單驗證
 const rules = reactive<FormRules>({
-  name: [
-    { required: true, message: "請輸入帳號", trigger: "blur" }, // 失去焦點時檢查
-    { pattern: /^(?=.*[a-z])(?=.*[A-Z]).{6,30}$/, message: "請輸入 6 位以上，須包含英文大小寫", trigger: "blur" } // https://blog.miniasp.com/post/2008/05/09/Using-Regular-Expression-to-validate-password
+  phone: [
+    { required: true, message: "請輸入手機號碼", trigger: "blur" }, // 失去焦點時檢查
+    { pattern: /^1\d{10}$/, message: "手機號碼必須是11位數字", trigger: "blur" }
+    // { pattern: /^(?=.*[a-z])(?=.*[A-Z]).{6,30}$/, message: "請輸入 6 位以上，須包含英文大小寫", trigger: "blur" } // https://blog.miniasp.com/post/2008/05/09/Using-Regular-Expression-to-validate-password
   ],
   password: [
     { required: true, message: "密碼不能為空", trigger: "blur" },
@@ -32,9 +42,9 @@ const rules = reactive<FormRules>({
     <!-- label-position top 標籤位置在上，size="large" 輸入框大小 -->
     <el-form :model="form" :rules="rules" ref="formRef" label-width="120px" label-position="top" size="large">
       <h2>登入</h2>
-      <!-- prop="name" 採用 rules name 的驗證規則 -->
-      <el-form-item label="帳號" prop="name">
-        <el-input v-model="form.name" />
+      <!-- prop="phone" 採用 rules phone 的驗證規則 -->
+      <el-form-item label="手機號碼" prop="phone">
+        <el-input v-model="form.phone" />
       </el-form-item>
       <el-form-item label="密碼" prop="password">
         <el-input v-model="form.password" />
